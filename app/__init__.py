bf51f7608aefffaf9b84e3062fb38eaecc144b4d
Egor <egorskurenkov391@gmail.com>
2026-02-12 00:36:15 +0300

    1

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,29 @@
-import eventlet
-eventlet.monkey_patch()
 from flask import Flask
 from .extentions import db, migrate
-from flask_socketio import SocketIO, send, emit, join_room, leave_room
+from flask_socketio import SocketIO
+
 socketio = SocketIO()
+
 
 def create_app(debug=True):
     from .routes.user import user
     from .routes.task import task
     from .routes.main import main
+
     app = Flask(__name__, template_folder='templates')
     app.config['socket_users'] = {}
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
     app.config['SECRET_KEY'] = '111'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     app.register_blueprint(user, url_prefix='/user')
     app.register_blueprint(task, url_prefix='/task')
     app.register_blueprint(main)
 
     db.init_app(app)
-    migrate.init_app(app,db)
+    migrate.init_app(app, db)
     socketio.init_app(app)
+
     with app.app_context():
         db.create_all()
 
--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
+import eventlet
+eventlet.monkey_patch()
+
 from app import create_app
 
-app1, app2 = create_app()
 
+socketio, app = create_app()
 
 if __name__ == '__main__':
-    app1.run(app2)
-
-
-
-
-
+    socketio.run(app, debug=True, port=5000)
